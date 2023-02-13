f6aa3c02036faf7f0a722f9c460883cedbf151e2
tkris <tkristian@my.wctc.edu>
2023-02-13 14:55:41 -0600

    [1.02] updated resp 1 to read text file

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 // Create movie Console Application to see all movies in the file and to add movies to the file Check for duplicate values!
 // Implement Exception Handling
 // Implement NLog framework
-String? resp = Console.ReadLine();
+string file = "movies.txt"; 
+string line;
+string? resp = Console.ReadLine();
 do{
 Console.WriteLine("Welcome to the Movie Library!\n");
 Console.WriteLine("What would you like to do?");
@@ -12,15 +14,19 @@ Console.WriteLine("To exit press any key.");
 resp = Console.ReadLine();
 
 if(resp == "1") {
-
-
+    if (File.Exists(file))
+    {
+        string content = File.ReadAllText(file);
+       Console.WriteLine(content);
+    }
 
 }
 
 else if (resp == "2"){
 
     Console.WriteLine("Enter title of movie: ");
-    Console.WriteLine("Enter movies genre (Can be more than one genre.): "); 
+
+
 
 }
 
